08afb6c2351d3db293159b4175b169768146fdb8
AjayAvatade <ajayavatade17@gmail.com>
2025-07-17 15:31:49 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,24 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div><section className="hero">
+        <div className="hero-content-wrapper">
+            <div className="hero-text">
+                <h1>Revolutionize Your <span>Pharmacy</span></h1>
+                <p>Streamline inventory, billing and patient care with our powerful Chemist ERP system.</p>
+                <div className="hero-cta">
+                    <Link to="/learnmore" className="primary-btn">LEARN MORE</Link>
+                    <Link to="#see-details" className="secondary-btn">SEE DETAILS</Link>
+                </div>
+            </div>
+            <div className="hero-image">
+                <img src={image} 
+                alt="Doctor using modern pharmacy ERP system with digital interface 
+                icons showing charts, prescriptions, and medical symbols"/>
+            </div>
+        </div> 
+    </section>
+    </div>
   )
 }
 
