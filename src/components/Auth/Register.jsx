2dff62138cb1edad09b0ec3df909d52562964d84
AjayAvatade <ajayavatade17@gmail.com>
2025-07-17 15:38:06 +0530

    Update Register.jsx

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -5,7 +5,30 @@ import React from 'react'
 
 function Register() {
   return (
-    <div>Register</div>
+    <div>
+    <div className="login-box">
+        <h2>REGISTER</h2>
+        <form action="/login">
+          <div className="form-login">
+            <label htmlFor="username">Email or Username</label>
+            <input type="text" id="username" name="username" required />
+          </div>
+          <div className="form-login">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <div className="form-login">
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input type="password" id="confirm-password" name="confirm-password" required />
+          </div>
+          <button type="submit" className="login-button">Sign Up</button>
+
+          <div id="forgot-password1" style={{marginTop: '20px'}}>
+            Already have an account? <Link to="/login">Log In</Link>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
