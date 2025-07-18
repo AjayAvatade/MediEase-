c1907b03d875eb4e713335cb298af824337bd819
AjayAvatade <ajayavatade17@gmail.com>
2025-07-18 14:02:36 +0530

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,6 +1,7 @@
 // Create a login form with email/password inputs. On submit, validate and call backend login API. Redirect to appropriate dashboard on success.
 
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Login() {
   return (
